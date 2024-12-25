fa1099b579bc40be5691861aa5a598e19eab5b2e
Zahi Ahmed <99167782+ZA-Projects@users.noreply.github.com>
2024-12-24 21:46:06 -0500

    Main

--- ISU Game/MasterMindMain.java
+++ ISU Game/MasterMindMain.java
@@ -8,7 +8,18 @@ class MasterMindMain
 
 	MasterMindMethods mmm = new MasterMindMethods ();
 
-	mmm.Login ();
+	// boolean pwdverify = mmm.Login ();
+	// if (pwdverify == true)
+	// {
+	//     System.out.println ("Password Accepted");
+	// }
+	// else
+	// {
+	//     System.out.println ("Password Incorrect");
+	// }
+	int code[] = mmm.CodeGeneration ();
+
+	int UserGuess[] = mmm.UserInput (code);
 
     }
 }
@@ -19,7 +30,7 @@ class MasterMindMethods
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
 
     boolean Login ()
-    throws java.io.IOException
+	throws java.io.IOException
     {
 	System.out.print ("What is your name?: ");
 	name = br.readLine ();
@@ -70,10 +81,42 @@ class MasterMindMethods
 	    System.out.print ("Create a password: ");
 	    password = br.readLine ();
 	    fw.write (password + "\r\n");
-	    fw.close();
+	    fw.close ();
 
 	    pwdverify = true;
 	}
 	return pwdverify;
     }
+
+
+    int[] CodeGeneration ()
+    {
+	int[] code = new int [5];
+	for (int count = 0 ; count < 5 ; count++)
+	{
+	    code [count] = (int) (Math.random () * 10) + 1;
+	}
+	return code;
+    }
+
+
+    int[] UserInput (int code[])
+	throws java.io.IOException
+    {
+	BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+	String input;
+	int[] UserGuess = new int [5];
+	System.out.println ("Guess the 5 digit code (Hint = its between 1-10)");
+	for (int count = 0 ; count < 5 ; count++)
+	{
+	    System.out.print ("Digit " + (count+1) + ":");
+	    input = br.readLine ();
+	    UserGuess [count] = Integer.parseInt (input);
+	    System.out.println();
+	}
+	return UserGuess;
+    }
 }
+
+
