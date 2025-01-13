9b647887133afe3f1c87bf23172ad3ea53f28000
Zahi Ahmed <99167782+ZA-Projects@users.noreply.github.com>
2025-01-13 18:18:58 -0500

    main

--- ISU Game/MasterMindMain.java
+++ ISU Game/MasterMindMain.java
@@ -7,6 +7,16 @@ class MasterMindMain
 	BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
 	MasterMindMethods mmm = new MasterMindMethods ();
 
+	boolean pwdverify = mmm.Login ();
+	if (pwdverify == true)
+	{
+	    System.out.println ("Password Accepted");
+	}
+	else
+	{
+	    System.out.println ("Password Incorrect");
+	}
+
 	int code[] = mmm.CodeGeneration ();
 	System.out.println ("Code generated (for debugging purposes):");
 	for (int count = 0 ; count < 5 ; count++)
@@ -60,7 +70,69 @@ class MasterMindMain
 
 class MasterMindMethods
 {
+    boolean pwdverify = true;
+    String name, password, input;
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+    boolean Login ()
+	throws java.io.IOException
+    {
+	System.out.print ("What is your name?: ");
+	name = br.readLine ();
+
+	System.out.print ("Have you played before(yes/no)?: ");
+	input = br.readLine ();
+	if (input.equalsIgnoreCase ("yes"))
+	{
+	    FileReader fr = new FileReader (name + ".txt");
+	    BufferedReader bfr = new BufferedReader (fr);
+	    password = bfr.readLine ();
+	    System.out.println ("Please enter your password: ");
+	    input = br.readLine ();
+	    if (!input.equals (password))
+	    {
+		System.out.print ("Password was Incorrect, please enter your password: ");
+		input = br.readLine ();
+
+		if (!input.equals (password))
+		{
+		    System.out.print ("Password was Incorrect, please enter your password: ");
+		    input = br.readLine ();
+
+		    if (!input.equals (password))
+		    {
+			pwdverify = false;
+		    }
+		    else
+		    {
+			pwdverify = true;
+		    }
+		}
+		else
+		{
+		    pwdverify = true;
+		}
+	    }
+	    else
+	    {
+		pwdverify = true;
+	    }
+
+	}
+	if (input.equalsIgnoreCase ("no"))
+	{
+	    FileWriter fw = new FileWriter (name + ".txt");
+
+	    System.out.print ("Create a password: ");
+	    password = br.readLine ();
+	    fw.write (password + "\r\n");
+	    fw.close ();
+
+	    pwdverify = true;
+	}
+	return pwdverify;
+    }
+
 
     int[] CodeGeneration ()
     {
@@ -124,7 +196,7 @@ class MasterMindMethods
 	    {
 		if (board [count] [count2] == -1)
 		{
-		    System.out.print ("_ "); 
+		    System.out.print ("_ ");
 		}
 		else
 		{
